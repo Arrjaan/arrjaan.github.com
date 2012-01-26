104c913e15beaafc8fe7960b7547188cf9c4a7b3
Arjan <arjanspoelstra@live.nl>
2012-01-26 19:19:44 +0100

    Vertalingen verbeterd.

--- lang.js
+++ lang.js
@@ -3,13 +3,16 @@ $(window).bind('hashchange', function() {
 	if ( lang == "#nl" ) nl();
 	if ( lang == "#en" ) en();
 });
+
 $(document).ready(function() {
 	var page = window.location.hash;
+	if ( lang == "#nl" ) nl();
+	if ( lang == "#en" ) en();
 });
 
 function en() {
 	document.getElementById("title").innerHTML = "<h2>Welcome</h2>";
-	document.getElementById("content").innerHTML = "Welcome to my GitHub page! More info is coming soon.";
+	document.getElementById("content_txt").innerHTML = "Welcome to my GitHub page! More info is coming soon.";
 	document.getElementById("sb_title").innerHTML = "<h2>Coming Soong</h2>";
 	document.getElementById("sb_content").innerHTML = "-";
 	document.getElementById("edited").innerHTML = "Last edited at";
@@ -17,7 +20,7 @@ function en() {
 
 function nl() {
 	document.getElementById("title").innerHTML = "<h2>Welkom</h2>";
-	document.getElementById("content").innerHTML = "Welkom op mijn GitHub pagina! Meer informatie volgt binnenkort.";
+	document.getElementById("content_txt").innerHTML = "Welkom op mijn GitHub pagina! Meer informatie volgt binnenkort.";
 	document.getElementById("sb_title").innerHTML = "<h2>Binnenkort</h2>";
 	document.getElementById("sb_content").innerHTML = "-";
 	document.getElementById("edited").innerHTML = "Laatst aangepast op";
