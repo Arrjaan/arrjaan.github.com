552f27ceac6a9028ba5f1a2ca959e3b1fc084f8a
Arjan Spoelstra <arjanspoelstra@live.nl>
2012-05-02 15:21:32 +0200

    Updated text and logo.

--- lang.js
+++ lang.js
@@ -12,16 +12,16 @@ $(document).ready(function() {
 
 function en() {
 	document.getElementById("title").innerHTML = "<h2>Welcome</h2>";
-	document.getElementById("content_txt").innerHTML = "Welcome to my GitHub page! More info is coming soon.";
-	document.getElementById("sb_title").innerHTML = "<h2>Coming Soong</h2>";
-	document.getElementById("sb_content").innerHTML = "-";
+	document.getElementById("content_txt").innerHTML = "Welcome to my GitHub page! More information is coming soon.";
+	document.getElementById("sb_title").innerHTML = "<h2>Information</h2>";
+	document.getElementById("sb_content").innerHTML = "I am currently working on project <a href=\"/Portall\">Portall</a>. All help is welcome!";
 	document.getElementById("edited").innerHTML = "Last edited at";
 }
 
 function nl() {
 	document.getElementById("title").innerHTML = "<h2>Welkom</h2>";
 	document.getElementById("content_txt").innerHTML = "Welkom op mijn GitHub pagina! Meer informatie volgt binnenkort.";
-	document.getElementById("sb_title").innerHTML = "<h2>Binnenkort</h2>";
-	document.getElementById("sb_content").innerHTML = "-";
+	document.getElementById("sb_title").innerHTML = "<h2>Informatie</h2>";
+	document.getElementById("sb_content").innerHTML = "Ik ben momenteel bezig met project <a href=\"/Portall\">Portall</a>. Hulp is van harte welkom!";
 	document.getElementById("edited").innerHTML = "Laatst aangepast op";
 }
